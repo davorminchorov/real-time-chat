ea2a6905273524962972cae3ac87b708a6e81850
Davor Minchorov <davorminchorov@gmail.com>
2015-11-12 14:28:12 +0100

    Finalized the web chat application

---
+++ public/js/app.js
@@ -0,0 +1,59 @@
+(function () {
+    'use strict';
+
+    var socket = io();
+
+
+    var $form = $('#message_form');
+    var $messageInput = $('#message');
+    var name = getQueryVariable('name') || 'Anonymous';
+    var room = getQueryVariable('room') || 'Public';
+    var $roomTitle = $('.room-title');
+
+    $roomTitle.text(room);
+
+
+    socket.on('connect', function () {
+        console.log('Connected to socket.io server!');
+        socket.emit('join-room', {
+            name: name,
+            room: room
+        });
+    });
+    
+    socket.on('message', function (message) {
+        var momentTimestamp = moment.utc(message.timestamp);
+        var $messages = $('.messages');
+        var $message = $('<li class="list-group-item"></li>');
+
+        $message.append('<p><strong>' + message.name + ' - ' + momentTimestamp.local().format('hh:mm a') + ':</strong></p>');
+        $message.append('<p>' + message.text + '</p>');
+        $messages.append($message);
+    });
+
+
+    $form.on('submit', function (event) {
+       event.preventDefault();
+
+        socket.emit('message', {
+            name: name,
+            text: $messageInput.val()
+        });
+
+        $messageInput.val('');
+
+    });
+
+    function getQueryVariable(variable) {
+        var query = window.location.search.substring(1);
+        var vars = query.split('&');
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split('=');
+            if (decodeURIComponent(pair[0]) == variable) {
+                return decodeURIComponent(pair[1]).replace(/\+/g, ' ');
+            }
+        }
+
+        return undefined;
+    }
+})();
--- server.js
+++ server.js
@@ -1,9 +1,87 @@
+'use strict';
+
 var PORT = process.env.port || 3000;
 var express = require("express");
 var app = express();
 var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var moment = require("moment");
 
 app.use(express.static(__dirname + '/public'));
+
+var clientInfo = {};
+
+function sendAllUsers(socket) {
+    var info = clientInfo[socket.id];
+    var users = [];
+
+    if(typeof info === 'undefined') {
+        return;
+    }
+
+    Object.keys(clientInfo).forEach(function (socketId) {
+       var userInfo = clientInfo[socketId];
+
+        if(info.room === userInfo.room) {
+            users.push(userInfo.name);
+        }
+
+    });
+
+    socket.emit('message', {
+        name: 'System',
+        text: 'List of all users: ' + users.join(', '),
+        timestamp: moment().valueOf()
+
+    });
+
+}
+
+io.on('connection', function (socket) {
+
+    socket.on('disconnect', function () {
+        var userData = clientInfo[socket.id];
+        if(typeof userData !== 'undefined') {
+            socket.leave(userData.room);
+            io.to(userData.room).emit('message', {
+                name: 'System',
+                text: userData.name + ' has left the room.',
+                timestamp: moment().valueOf()
+            });
+
+            delete clientInfo[socket.id];
+        }
+    });
+
+    socket.on('join-room', function (request) {
+
+        clientInfo[socket.id] = request;
+        socket.join(request.room);
+        socket.broadcast.to(request.room).emit('message', {
+            name: 'System',
+            text: request.name + ' has joined the room!',
+            timestamp: moment().valueOf()
+        });
+    });
+
+    socket.on('message', function (message) {
+
+        if(message.text === '@currentUsers') {
+            sendAllUsers(socket);
+        } else {
+            message.timestamp = moment().valueOf();
+            io.to(clientInfo[socket.id].room).emit('message', message);
+        }
+
+    });
+
+    socket.emit('message', {
+        name: 'System',
+        text: 'Welcome to the chat application!',
+        timestamp: moment().valueOf()
+    });
+
+});
 
 http.listen(PORT, function () {
     console.log('Server started at port ' + PORT);
